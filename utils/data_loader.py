8483d1896f5514a5049232f1764724bfc99049b5
sanjanamt2006-cloud <sanjanamt2006@gmail.com>
2026-06-06 10:53:41 +0530

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 def load_data():
     return pd.read_csv(
-        "data/student_placement_prediction_dataset_2026.csv"
+        "data/student_placement_prediction_dataset_2026[1].csv"
     )
